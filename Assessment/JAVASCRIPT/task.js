b88224d68657e33aa424f2ec6abd6dc924e183b4
ayushpurohit9426 <ayushpurohit2006@gmail.com>
2025-09-18 20:51:11 +0530

    js

---
+++ Assessment/JAVASCRIPT/task.js
@@ -0,0 +1,7 @@
+// utils.js
+export function setItem(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+export function getItem(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
