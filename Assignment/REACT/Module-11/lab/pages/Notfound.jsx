b036190c08e3df1c069029ba37b7620af1f3614e
ayushpurohit9426 <ayushpurohit2006@gmail.com>
2025-11-07 09:42:16 +0530

    react

---
+++ Assignment/REACT/Module-11/lab/pages/Notfound.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useNavigate } from 'react-router'
+
+function Notfound() {
+
+    const redirect = useNavigate()
+
+    const back = ()=>{
+        redirect("/")
+    }
+
+
+  return (
+    <div className='text-center'>
+        <h1 className=' text-capitalize text-danger'>Error 404 Not Found</h1>
+        <button onClick={back} className='btn btn-outline-danger'>BACK</button>
+    </div>
+  )
+}
+
+export default Notfound
